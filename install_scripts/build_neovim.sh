64d85d4b0c72a8527df2828e0f648d03a92f60ce
Steffen Baranowsky <BaraMGB@arcor.de>
2024-04-10 01:45:40 +0200

    remove sudo from installing nvim

--- install_scripts/build_neovim.sh
+++ install_scripts/build_neovim.sh
@@ -5,4 +5,4 @@ mkdir -p Projects
 cd ~/Projects/
 git clone https://github.com/neovim/neovim.git
 cd neovim
-make CMAKE_BUILD_TYPE=RelWithDebInfo && sudo make install
+make CMAKE_BUILD_TYPE=RelWithDebInfo && make install
