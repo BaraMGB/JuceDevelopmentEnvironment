80c907e6fbf8ddcf54ce1a41445ecd920d2c9212
Steffen Baranowsky <BaraMGB@freenet.de>
2024-04-10 23:14:13 +0200

    Update install_setupJuceProject.sh

--- install_scripts/install_setupJuceProject.sh
+++ install_scripts/install_setupJuceProject.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+if [ "$SUDO_USER" ]; then
+    HOME_DIR=$(getent passwd "$SUDO_USER" | cut -d: -f6)
+else
+    HOME_DIR=$HOME
+fi
+
 git clone https://github.com/BaraMGB/setup_juce_project.git 
 
-mkdir -p ~/bin
-ln -s $(pwd)/setup_juce_project/setup_juce_project.sh ~/bin/setup_juce_project
+mkdir -p $HOME_DIR/bin
+ln -s $(pwd)/setup_juce_project/setup_juce_project.sh $HOME_DIR/bin/setup_juce_project
 
