353ca4c670af645b2d933b7d628b18ce1b2f023c
Steffen Baranowsky <BaraMGB@freenet.de>
2024-04-10 23:12:28 +0200

    Update build_neovim.sh

--- install_scripts/build_neovim.sh
+++ install_scripts/build_neovim.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+if [ "$SUDO_USER" ]; then
+    HOME_DIR=$(getent passwd "$SUDO_USER" | cut -d: -f6)
+else
+    HOME_DIR=$HOME
+fi
+
 cd
 mkdir -p Projects
-cd ~/Projects/
+cd $HOME_DIR/Projects/
 git clone https://github.com/neovim/neovim.git
 cd neovim
 make CMAKE_BUILD_TYPE=RelWithDebInfo && make install
