e6f642a9e6f0cd130f57384ab4cffe5b2f729d29
Steffen Baranowsky <BaraMGB@freenet.de>
2024-04-10 23:13:20 +0200

    Update install_nerd_font.sh

--- install_scripts/install_nerd_font.sh
+++ install_scripts/install_nerd_font.sh
@@ -1,9 +1,16 @@
 #/bin/bash
 # install DroidSansMono Nerd Font --> u can choose another at: https://www.nerdfonts.com/font-downloads
+
+if [ "$SUDO_USER" ]; then
+    HOME_DIR=$(getent passwd "$SUDO_USER" | cut -d: -f6)
+else
+    HOME_DIR=$HOME
+fi
+
 echo "[-] Download fonts [-]"
 echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip"
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip
-unzip DroidSansMono.zip -d ~/.fonts
+unzip DroidSansMono.zip -d $HOME_DIR/.fonts
 fc-cache -fv
 rm DroidSansMono.zip
 echo "done!"
